de91074046139ef2f74065c7f1e4a14f6071939f
ShradhaNbr <sradha.nambiar@gmail.com>
2021-06-30 16:15:04 +0530

    Refactoring the code to add part time

--- empwagecalculation.java
+++ empwagecalculation.java
@@ -2,17 +2,29 @@ public class empwagecalculation {
 public static void main(String args[])
 {
 System.out.println("Welcome to Employee Wage computation");
-int IS_FULL_TIME =1;
+int IS_FULL_TIME=1;
+int IS_PART_TIME=2;
 int wage_per_hour=20;
 int work_hour;
-    double empcheck = Math.floor(Math.random() * 10) % 2;
+int wage;
+    double empcheck = Math.floor(Math.random() * 10) % 3;
     if ( empcheck == IS_FULL_TIME )
     {
     System.out.println("Employee is Present");
     work_hour=8;
     }
+    else if ( empcheck == IS_PART_TIME )
+    {
+    System.out.println("Employee is present part time");
+    work_hour=4;
+    }
     else
+    {
     System.out.println("Employee is Absent");
     work_hour=0;
+    }
+wage = wage_per_hour * work_hour;
+System.out.println("Wage of the employee is" + wage);
+
 }
 }
