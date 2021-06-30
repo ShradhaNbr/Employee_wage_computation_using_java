ebd131c6e41aff1a418885743b5637b823dcf78d
ShradhaNbr <sradha.nambiar@gmail.com>
2021-06-30 17:01:06 +0530

    Refactoring code to calculate till working hours is reached to 100 or days is reached 20

--- empwagecalculation.java
+++ empwagecalculation.java
@@ -5,11 +5,11 @@ System.out.println("Welcome to Employee Wage computation");
 int IS_FULL_TIME=1;
 int IS_PART_TIME=2;
 int wage_per_hour=20;
-int work_hour;
+int work_hour=0;
 int wage;
 int days_worked=0;
 int monthly_wage=0;
-while (days_worked<=20)
+while (days_worked<=20 && work_hour<=100 )
 {
 int empcheck = (int) (Math.random() * 10) % 3;
     switch (empcheck)
@@ -36,6 +36,7 @@ wage = wage_per_hour * work_hour;
 System.out.println("Wage of the employee is" + wage);
 monthly_wage = wage * days_worked;
 days_worked++;
+work_hour++;
 }
 System.out.println("Monthly wage is" + monthly_wage);
 }
