dbd4a4bfe1f3b71157d0138096cf1c52cce96956
ShradhaNbr <sradha.nambiar@gmail.com>
2021-06-30 15:57:48 +0530

    Refactoring the code to calculate wage

--- empwagecalculation.java
+++ empwagecalculation.java
@@ -3,10 +3,16 @@ public static void main(String args[])
 {
 System.out.println("Welcome to Employee Wage computation");
 int IS_FULL_TIME =1;
+int wage_per_hour=20;
+int work_hour;
     double empcheck = Math.floor(Math.random() * 10) % 2;
     if ( empcheck == IS_FULL_TIME )
+    {
     System.out.println("Employee is Present");
+    work_hour=8;
+    }
     else
     System.out.println("Employee is Absent");
+    work_hour=0;
 }
 }
