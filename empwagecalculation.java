a25f321126de15d5603b8c225130b6a5941fac1c
ShradhaNbr <sradha.nambiar@gmail.com>
2021-06-30 16:33:57 +0530

    Refactoring the code to use switch

--- empwagecalculation.java
+++ empwagecalculation.java
@@ -7,22 +7,27 @@ int IS_PART_TIME=2;
 int wage_per_hour=20;
 int work_hour;
 int wage;
-    double empcheck = Math.floor(Math.random() * 10) % 3;
-    if ( empcheck == IS_FULL_TIME )
-    {
-    System.out.println("Employee is Present");
-    work_hour=8;
-    }
-    else if ( empcheck == IS_PART_TIME )
-    {
-    System.out.println("Employee is present part time");
-    work_hour=4;
-    }
-    else
-    {
+int empcheck = (int) (Math.random() * 10) % 3;
+    switch (empcheck)
+	{
+	case 1:
+    	{
+    		System.out.println("Employee is Present");
+		    work_hour=8;
+			break;
+    	}
+    	case 2:
+    	{
+	        System.out.println("Employee is present part time");
+    		work_hour=4;
+			break;
+    	}
+    default:
+    	{
     System.out.println("Employee is Absent");
     work_hour=0;
-    }
+    	}
+	}
 wage = wage_per_hour * work_hour;
 System.out.println("Wage of the employee is" + wage);
 
