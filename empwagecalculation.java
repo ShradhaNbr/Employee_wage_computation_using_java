6e7ad6d04de004e9a62859a419a709cacd1180ee
ShradhaNbr <sradha.nambiar@gmail.com>
2021-06-30 15:48:27 +0530

    Refactoring the code to check whether employee is present or not

--- empwagecalculation.java
+++ empwagecalculation.java
@@ -1,6 +1,12 @@
 public class empwagecalculation {
 public static void main(String args[])
 {
-	System.out.print("Welcome to Employee Wage Computation");
+System.out.println("Welcome to Employee Wage computation");
+int IS_FULL_TIME =1;
+    double empcheck = Math.floor(Math.random() * 10) % 2;
+    if ( empcheck == IS_FULL_TIME )
+    System.out.println("Employee is Present");
+    else
+    System.out.println("Employee is Absent");
 }
 }
