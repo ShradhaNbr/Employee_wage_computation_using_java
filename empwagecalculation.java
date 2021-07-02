2e1f37a93aad267aad120d136b9cfb1295535579
ShradhaNbr <sradha.nambiar@gmail.com>
2021-07-02 13:34:47 +0530

    Refactoring the code to calculate daily wage

--- empwagecalculation.java
+++ empwagecalculation.java
@@ -5,10 +5,13 @@ public class empwagecalculation {
 		System.out.println("Welcome to Employee Wage computation");
 		int emp_rate_per_hour=20;
 		int emp_hours;
+		int emp_wage;
     		double empcheck = Math.floor(Math.random() * 10) % 2;
 	        if ( empcheck == IS_FULL_TIME )
 		    emp_hours=8;
     		else
-		    emp_hours=4;
+		    emp_hours=0;
+		emp_wage= emp_hours * emp_rate_per_hour;
+		System.out.println("Employee wage" + emp_wage);
 	}
 }
