06322a2b3d3c684ec179a95be2e96148367c09d4
ShradhaNbr <sradha.nambiar@gmail.com>
2021-07-02 14:38:20 +0530

    Refactoring code to calculate hours worked in a month

--- empwagecalculation.java
+++ empwagecalculation.java
@@ -1,43 +1,33 @@
 public class empwagecalculation {
-public static void main(String args[])
-{
-System.out.println("Welcome to Employee Wage computation");
-int IS_FULL_TIME=1;
-int IS_PART_TIME=2;
-int wage_per_hour=20;
-int work_hour=0;
-int wage;
-int days_worked=0;
-int monthly_wage=0;
-while (days_worked<=20 && work_hour<=100 )
-{
-int empcheck = (int) (Math.random() * 10) % 3;
-    switch (empcheck)
-	{
-	case 1:
-    	{
-    		System.out.println("Employee is Present");
-		    work_hour=8;
-			break;
-    	}
-    	case 2:
-    	{
-	        System.out.println("Employee is present part time");
-    		work_hour=4;
-			break;
-    	}
-    default:
-    	{
-    System.out.println("Employee is Absent");
-    work_hour=0;
-    	}
+
+	public static final int IS_FULL_TIME =1;
+	public static final int IS_PART_TIME =2;
+	public static void main(String args[]) {
+		System.out.println("Welcome to Employee Wage computation");
+		int emp_rate_per_hour=20;
+		int emp_hours;
+		int emp_wage;
+		int working_days=20;
+		int total_emp_wage=0;
+		int working_hrs=100;
+		int total_emp_hrs=0;
+		int total_working_days=0;
+		while(total_emp_hrs < working_hrs && total_working_days < working_days ) {
+	    		int empcheck = (int) Math.floor(Math.random() * 10) % 3;
+			switch (empcheck) {
+	        		case IS_FULL_TIME:
+				    emp_hours=8;
+				    break;
+				case IS_PART_TIME:
+				    emp_hours=4;
+				    break;
+	    			default:
+			    	    emp_hours=0;
+			}
+			total_emp_hrs+=emp_hours;
+			total_emp_wage=total_emp_hrs * emp_rate_per_hour;
+			System.out.println("Total hours worked" + total_emp_hrs);
+		}
+		System.out.println("Total Emp Wage" + total_emp_wage);
 	}
-wage = wage_per_hour * work_hour;
-System.out.println("Wage of the employee is" + wage);
-monthly_wage = wage * days_worked;
-days_worked++;
-work_hour++;
-}
-System.out.println("Monthly wage is" + monthly_wage);
-}
 }
